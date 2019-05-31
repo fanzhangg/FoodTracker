9a8b8152f2110e266709033cc6a29737525cfca3
Fan Zhang <vanadiumzhang@gmail.com>
2019-05-31 14:55:40 +0800

    Change the condition to initialize `Meal` object to be `nil`

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -11,21 +11,26 @@ import UIKit
 class Meal {
     // MAKR: properties
     var name: String
-    var photo: UIImage
+    var photo: UIImage?
     var rating: Int
     
     // MARK: Initialization
     // - `init?`: failabel initializer, return optional value / implicitly unwrapped optional values (a valid value / nil)
     // - The initializer returns an optional `Meal?` object
     init?(name: String, photo: UIImage?, rating: Int) {
-        // Initialization fails if there is no name or if the rating is negative
-        if name.isEmpty || rating < 0 {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating is between 0 and 5 exclusively
+        guard (rating >= 0) && (rating <= 5) else {
             return nil
         }
         
         // Initialize stored properties
         self.name = name
-        self.photo = photo!
+        self.photo = photo
         self.rating = rating
     }
 }
